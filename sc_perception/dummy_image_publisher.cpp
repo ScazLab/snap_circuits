27c4174004cf60ce25fcdb0740eb40b07873ef22
Alessandro Roncone <alecive87@gmail.com>
2016-04-22 19:10:01 -0400

    Added couple of more parts as svg images

--- sc_perception/dummy_image_publisher.cpp
+++ sc_perception/dummy_image_publisher.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv)
 {
-    std::string name="dummy_image_pub";
+    std::string name="dummy_image_publisher";
 
     ros::init(argc, argv, name.c_str());
     ros::NodeHandle nH;
@@ -15,8 +15,6 @@ int main(int argc, char** argv)
     nH.param<std::string>(("/"+name+"/pub").c_str(), pub, "/snap_circuits/image_undistorted");
 
     std::string filename="";
-    nH.param<std::string>(("/"+name+"/filename").c_str(), filename, 
-                          "/home/alecive/code/catkin_my_ws/src/snap_circuits/frame0005.jpg");
        
     // Dirty way to process command line arguments. It seems that
     // there is not a straightforward standard ROS way, unfortunately.
@@ -30,17 +28,37 @@ int main(int argc, char** argv)
     ROS_INFO("[dummyImagePublisher] Filename   set to %s", filename.c_str());
     ROS_INFO("[dummyImagePublisher] Publishing     to %s", pub.c_str());
 
-    cv_bridge::CvImage msgOut;
-    msgOut.encoding = sensor_msgs::image_encodings::BGR8;
-    msgOut.image    = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
-
     ros::Publisher imgPub = nH.advertise<sensor_msgs::Image>(pub.c_str(), 1);
     ros::Rate loop_rate(1);
+    loop_rate.sleep();
 
     while (nH.ok()) 
     {
-        ROS_INFO("[dummyImagePublisher] Publishing!");
-        imgPub.publish(msgOut);
+        std::string new_file;
+        nH.param<std::string>(("/"+name+"/filename").c_str(), new_file, filename); 
+
+        if (filename!=new_file)
+        {
+            filename=new_file;
+
+            cv_bridge::CvImage msgOut;
+            msgOut.encoding = sensor_msgs::image_encodings::BGR8;
+
+            cv::Mat mat = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
+
+            if (!mat.data)
+            {
+                ROS_ERROR("Could not find or import the image");
+            }
+            else
+            {
+                msgOut.image = mat;
+
+                ROS_INFO("[dummyImagePublisher] Publishing image from %s",new_file.c_str());
+                imgPub.publish(msgOut);
+            }
+        }
+        
         loop_rate.sleep();
     }
 
