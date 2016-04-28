c103547965cfa50cedfd5db50acaf444f7d8d982
Olivier Mangin <olivier@mangin.com>
2016-04-28 17:03:16 -0400

    Integrates part classification python code into catkin build.

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['tutorial_package'],
-    package_dir={'': 'src'},
+    packages=['board_perception'],
 )
 
 setup(**setup_args)
